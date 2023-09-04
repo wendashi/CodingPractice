f6ae9b994ef9a8fb13faf7ae6a43f3b6ca8956ae
wendashi <stoneshaintshi@gmail.com>
2023-09-04 20:57:15 +0800

    2023.9.4

--- DataStructure_StackandQueue/20-ValidParentheses.py
+++ DataStructure_StackandQueue/20-ValidParentheses.py
@@ -3,22 +3,26 @@ class Solution():
         stack = []
 
         for i in s:
+            print(stack)
             if i == '(':
                 stack.append(')')
             elif i == '[':
                 stack.append(']')
             elif i == '{':
                 stack.append('}')
-            elif not stack or stack[-1] != i:
-                return False
+            # elif not stack or stack[-1] != i:
+            #     return False
+            # else:
+            #     stack.pop()
+            # else 分支中的 stack.pop() 操作将在每次执行 else 时都执行
             else:
-                stack.pop()
-
-
+                if not stack or i != stack.pop(): 
+                    return False    
+                
         return True if not stack else False
 
 sol = Solution()
 
-res = sol.vp(s = '(){}[')
+res = sol.vp(s = '(()))')
 
 print(res)
