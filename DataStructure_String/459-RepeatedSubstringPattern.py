2a119d2059eae85c67943f95a394740710e2f089
wendashi <stoneshaintshi@gmail.com>
2023-09-09 02:14:16 +0800

    2023.9.9

--- DataStructure_String/459-RepeatedSubstringPattern.py
+++ DataStructure_String/459-RepeatedSubstringPattern.py
@@ -1,12 +1,42 @@
-class Solutioin():
-    def repeat(self, s: str):
-        n = len(s)
-        for i in range(1, n//2 + 1):
-            if n % i == 0 and s[:i] * (n//i) == s[:] :
-                return True
+# class Solutioin():
+#     def repeat(self, s: str):
+#         n = len(s)
+#         for i in range(1, n//2 + 1):
+#             if n % i == 0 and s[:i] * (n//i) == s[:] :
+#                 return True
         
+#         return False
+
+# sol = Solutioin()
+# res = sol.repeat(s = "abab")
+# print(res)
+
+
+class Solution():
+    def repeatedsp(self, s: str):
+        n = len(s)
+        if n <= 1:
+            return False
+        
+        for i in range(1, (n // 2) + 1):
+            if n % i == 0 and s[:i] * (n//i) == s:
+                return True
+
         return False
 
-sol = Solutioin()
-res = sol.repeat(s = "abab")
+sol = Solution()
+res = sol.repeatedsp(s = "ababb")
 print(res)
+
+# 时间复杂度分析：
+
+# 循环从 i = 1 到 (n // 2) + 1 进行迭代，其中 n 表示输入字符串 s 的长度。
+# 在每次迭代中，进行了一些常数时间的操作，包括整数除法，字符串切片，字符串乘法和字符串比较。
+# 循环的迭代次数取决于 n 的大小，但是它不会遍历整个字符串 s。
+# 因为循环的迭代次数与输入规模 n 相关，所以该算法的时间复杂度是 O(n)。
+
+# 空间复杂度分析：
+
+# 除了循环变量 i 和输入参数 s 之外，代码没有使用额外的数据结构或内存空间。
+# 字符串切片和乘法操作不会显式创建新的字符串，因此它们不会显著增加空间复杂度。
+# 因此，该算法的空间复杂度是 O(1)，即它的空间消耗是常数级的，与输入规模无关。
--- DataStructure_String/Offer58-IIreverseLeftWords.py
+++ DataStructure_String/Offer58-IIreverseLeftWords.py
@@ -1,7 +1,32 @@
-class Solution():
-    def reverseLeftWords(self, s: str, k: int):
-        return s[k:] + s[:k]
+# class Solution():
+#     def reverseLeftWords(self, s: str, k: int):
+#         return s[k:] + s[:k]
     
+# sol = Solution()
+# res = sol.reverseLeftWords(s = "abcdefg", k = 2)
+# print(res)
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        new_s = ''
+        for i in range(len(s)):
+            j = (i + n) % len(s)
+            new_s = new_s + s[j]
+        
+        return ''.join(new_s)
+
 sol = Solution()
-res = sol.reverseLeftWords(s = "abcdefg", k = 2)
+res = sol.reverseLeftWords(s = "abcdefg", n = 2)
 print(res)
+
+# 时间复杂度分析：O(n)
+
+# 主要的时间消耗来自于循环中的迭代，循环遍历了字符串s的每个字符，共执行了len(s)次迭代。
+# 在每次迭代中，进行了一些常数时间的操作，如索引计算、字符串拼接。
+# 所以总体时间复杂度可以表示为O(len(s))，其中len(s)是输入字符串s的长度。
+
+# 空间复杂度分析：O(n)
+
+# 主要的空间消耗来自于创建了一个名为new_s的新字符串，它的长度等于输入字符串s的长度。
+# 此外，还有一些常数级的辅助空间消耗，如循环变量i、j等。
+# 所以总体空间复杂度可以表示为O(len(s))，其中len(s)是输入字符串s的长度。
