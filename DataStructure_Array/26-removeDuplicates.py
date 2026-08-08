d1d6e72d4b9de23a8cd19b5135ccbe8b009bafb4
wendashi <stoneshaintshi@gmail.com>
2026-08-08 17:56:46 +0800

    2026-08-08 17:56:00 88-2nd

--- DataStructure_Array/26-removeDuplicates.py
+++ DataStructure_Array/26-removeDuplicates.py
@@ -9,7 +9,7 @@ formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 # 打印当前时间
 print("当前时间是:", formatted_time)
 
-# 当前时间是: 2023-09-22 12:16:08
+# 当前时间是: 2026-08-07 23:36:34
 # range 是左闭右开
 
 class Solution():
@@ -25,5 +25,3 @@ class Solution():
 sol = Solution()
 res = sol.removeduplicate(nums = [0,0,1,1,1,2,2,3,3,4])
 print(res)
-
-# 当前时间是: 2023-09-22 12:27:25
--- DataStructure_Array/88-MergeSortedArray.py
+++ DataStructure_Array/88-MergeSortedArray.py
@@ -1,63 +1,3 @@
-# class Solution():
-#     def merge(self, nums1:list[int], m:int, nums2:list[int], n:int):
-#         sorted = []
-#         p1, p2 = 0, 0
-#         while p1 < m or p2 < n :
-#             print('??')
-#             if p1 == m:
-#                 sorted.append(nums2[p2])
-#                 p2 += 1
-#                 print(3)
-#             elif p2 == n:
-#                 sorted.append(nums1[p1])
-#                 p1 += 1
-#                 print(4)    
-#             elif nums1[p1] < nums2[p2]:
-#                 sorted.append(nums1[p1])
-#                 p1 += 1
-#                 print(1)
-#             elif nums1[p1] >= nums2[p2]:#else:
-#                 sorted.append(nums2[p2]) 
-#                 p2 += 1        
-#         nums1[:] = sorted
-#         print(nums1)
-
-# Solution_instance = Solution()
-
-# res = Solution_instance.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
-
-# print(res)
-
-#2023.9.20(7th)
-
-# class Solution():
-#     def merge(self, nums1: list[int], m: int, nums2: list[int], n :int):
-#         temp = []
-#         p1, p2 = 0, 0
-#         while p1 < m or p2 < n:
-#             if p2 == n:
-#                 temp.append(nums1[p1])
-#                 p1 +=1               
-#             elif p1 == m:
-#                 temp.append(nums2[p2])
-#                 p2 += 1    
-#             elif nums1[p1] > nums2[p2]:
-#                 temp.append(nums2[p2])
-#                 p2 += 1   
-#             else:
-#                 temp.append(nums1[p1])
-#                 p1 += 1
-        
-#         nums1[:] = temp[:]
-
-#         return nums1
-    
-# sol = Solution()
-# res = sol.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
-# print(res)
-
-# 2023.9.21(8th)
-
 import datetime
 
 # 获取当前的日期和时间
@@ -69,60 +9,57 @@ formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 # 打印当前时间
 print("当前时间是:", formatted_time)
 
-# 2023-09-21 11:59:30
+# 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+# 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+# 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+
 
 # class Solution():
 #     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int):
-#         p1 = 0
-#         p2 = 0
-#         res = []
-#         while p1 < m or p2 < n:
-#             if p1 == m:
-#                 res.append(nums2[p2])
-#                 p2 += 1
-#             elif p2 == n:
-#                 res.append(nums1[p1])
-#                 p1 += 1
-#             elif nums1[p1] > nums2[p2]:
-#                 res.append(nums2[p2])
-#                 p2 += 1
+#         i = m - 1 # nums1 有效部分末尾
+#         j = n - 1 # nums2 末尾
+#         write = m + n - 1  # 指向 nums1 实际末尾，这里就是放 nums1[i] 和 nums2[j] 中最大的
+        # 🔥如果是 nums1 先完、还要继续比 nums2
+        # 如果是 nums2 先完，说明 nums1 的本来就小、不用动
+#         while j >= 0: 
+#             if i >= 0 and nums1[i] > nums2[j]:
+#                 nums1[write] = nums1[i]
+#                 i -= 1
 #             else:
-#                 res.append(nums1[p1])
-#                 p1 += 1
+#                 nums1[write] = nums2[j]
+#                 j -= 1
+#             write -= 1
 
-#         nums1[:] = res[:]
 #         return nums1
 
 # sol = Solution()
 # res = sol.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
 # print(res)
 
-# # 当前时间是: 2023-09-21 12:29:38
-
-# 当前时间是: 2023-09-26 10:40:09（8th）
 
 class Solution():
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int):
-        res = []
-        i, j = 0, 0
-        while i < m or j < n:
-            if i == m:
-                res.append(nums2[j])
-                j += 1
-            elif j == n:
-                res.append(nums1[i])
-                i += 1
-            elif nums1[i] < nums2[j]:
-                res.append(nums1[i])
-                i += 1
+        i = m - 1 # nums1 非0的实际末尾
+        j = n - 1 # nums2 末尾
+        k = m + n - 1 # nums1 的 0 末尾
+
+        # 如果 nums2 走完, nums1 没走完，说明nums1的本来就更小
+        # ❌ 如果 nums1 走完，nums2 没走完
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                res.append(nums2[j])
-                j += 1
-            
-        nums1[:] = res[:]
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
         return nums1
 
+
+
 sol = Solution()
-res = sol.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
+res = sol.merge(nums1 = [2,0], m = 1, nums2 = [1], n = 1)
 print(res)
