7329f69626a691ae5520dab495b596a8a8ff9de4
wendashi <stoneshaintshi@gmail.com>
2026-08-09 15:06:12 +0800

    2026-08-09 13:22:49 58-1st

--- DataStructure_Array/26-removeDuplicates.py
+++ DataStructure_Array/26-removeDuplicates.py
@@ -22,6 +22,11 @@ class Solution():
         
         return slow + 1
 
+# - 时间复杂度 ：代码运行得有多快，本质上看“操作次数随 n 怎么增长”
+# for fast in range(len(nums)), 循环次数和 n = len(nums) 成正比, 所以时间复杂度是 O(n) 。
+# - 空间复杂度 ：代码额外占了多少内存，本质上看“额外开了多少空间”
+# 空间复杂度 这里只用了几个额外变量： slow 、 fast ，没有开新数组。 虽然改了 nums ，但那是 原地修改 ，不算额外空间。 所以空间复杂度是 O(1) 。
+
 sol = Solution()
 res = sol.removeduplicate(nums = [0,0,1,1,1,2,2,3,3,4])
 print(res)
---
+++ DataStructure_Array/58-LengthOfLastWord.py
@@ -0,0 +1,46 @@
+# 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+
+# 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+
+# 示例 1：
+# 输入：s = "Hello World"
+# 输出：5
+# 解释：最后一个单词是“World”，长度为 5。
+
+# 示例 2：
+# 输入：s = "   fly me   to   the moon  "
+# 输出：4
+# 解释：最后一个单词是“moon”，长度为 4。
+
+# 示例 3：
+# 输入：s = "luffy is still joyboy"
+# 输出：6
+# 解释：最后一个单词是长度为 6 的“joyboy”。
+# https://leetcode.cn/problems/length-of-last-word/description/
+
+from collections import defaultdict
+import datetime
+
+# 获取当前的日期和时间
+current_time = datetime.datetime.now()
+
+# 将日期和时间格式化为字符串
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+# 打印当前时间
+print("当前时间是:", formatted_time)
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1 # 最后一个单词的最后一个字符位置
+        while s[i] == ' ': # 把末尾空格都跳过去
+            i -= 1
+
+        j = i # 从最后一个单词的右边开始
+        while j >= 0 and s[j] != ' ':
+            j -= 1
+
+        return i - j
+
+s = "Hello World"
+print(len(s))
