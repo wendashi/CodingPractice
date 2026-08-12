0d744126a47098ab62e8ec5558804682ec1378b5
wendashi <stoneshaintshi@gmail.com>
2026-08-13 00:11:07 +0800

    当前时间是: 2026-08-13 00:10:26 344-7th

--- DataStructure_String/344-ReverseString.py
+++ DataStructure_String/344-ReverseString.py
@@ -1,42 +1,4 @@
 # class Solution():
-#     def rever(self, s: list[str]):
-#         """
-#         Do not return anything, modify s in-place instead.
-#         """
-#         s[:] = [s[i] for i in range(len(s)-1, -1, -1)]
-#         # return s[:]
-        
-
-# sol = Solution()
-# res = sol.rever(s = ["h","e","l","l","o"])
-# print(res)
-
-## 2023.9.8 two pointers
-
-# class Solution():
-#     def reverstring(self, s: list[str]):
-#         l = 0
-#         r = len(s) - 1
-#         while l < r:
-#             # tempr = s[r]
-#             # templ = s[l]
-#             # s[l] = tempr
-#             # s[r] = templ
-#             s[l], s[r] = s[r], s[l]
-#             l += 1
-#             r -= 1
-            
-
-#         return s[:]
-    
-
-# sol = Solution()
-# res = sol.reverstring(s = ["h","e","l","l","o"])
-# print(res)
-
-# 2023.9.16(6th)
-
-# class Solution():
 #     def reversestring(self, s: list[str]):
 #         # return s[::-1] 
 #         l = 0
@@ -46,14 +8,7 @@
 #             l += 1
 #             r -= 1
         
-#         return s[:]
-
-
-
-# sol = Solution()
-# res = sol.reversestring(s = ["h","e","l","l","o"])
-# print(res)
-# #time: O(n), space: O(1)
+#         return s
 
 
 import datetime
@@ -71,6 +26,18 @@ print("当前时间是:", formatted_time)
 
 class Solution():
     def rever(self, s: str):
-        s[:] = s[::-1]
+        # s[:] = s[::-1]
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+           temp = s[left]
+           s[left] = s[right]
+           s[right] = temp
+           left += 1
+           right -= 1
+        
+        return s
     
 sol = Solution
