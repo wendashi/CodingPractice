facb980f0ae099d350e9e59ce9bbb0ed7de88a65
wendashi <stoneshaintshi@gmail.com>
2026-08-14 23:06:04 +0800

    2026-08-14 23:05:46 459-7th

--- DataStructure_HashTable/18-4Sum.py
+++ DataStructure_HashTable/18-4Sum.py
@@ -18,54 +18,7 @@ formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 print("当前时间是:", formatted_time)
 
 # 当前时间是: 2023-09-29 21:09:20
-
-# class Solution():
-#     def foursum(self, nums: list[int], target: int):
-#         nums.sort()
-#         res = []
-#         for i in range(len(nums)):
-#             print('i', nums[i])
-#             if nums[i] > target and target > 0:
-#                 break
-            
-#             # 避免同样的第一个数重复开头
-#             if i > 0 and nums[i] == nums[i - 1]:
-#                 continue
-
-#             for j in range(i + 1, len(nums)):
-#                 print('j', nums[j])
-#                 if nums[i] + nums[j] > target and target > 0:
-#                     break
-
-#                 # 避免同样的第二个数重复开头
-#                 if j > i + 1 and nums[j] == nums[j - 1]:
-#                     continue
-
-#                 l = j + 1
-#                 r = len(nums) - 1
-
-#                 # print(l, r)
-#                 while l < r:
-#                     sum = nums[i] + nums[j] + nums[l] + nums[r]
-#                     # print(([nums[i], nums[j], nums[l], nums[r]]))
-#                     # print('sum', sum)
-#                     if sum > target:
-#                         r -= 1
-#                     elif sum < target:
-#                         l += 1
-#                     else:
-#                         # print('???????')
-#                         res.append([nums[i], nums[j], nums[l], nums[r]])
-#                         # print('res', res)
-
-#                         while l < r and nums[l] == nums[l + 1]:
-#                             l += 1
-#                         while l < r and nums[r] == nums[r - 1]:
-#                             r -= 1
-#                         l += 1
-#                         r -= 1
-        
-#         return res
+# 2026-08-14 22:23:40
 
 class Solution:
     def fourSum(self, nums: List[int], target: int):
@@ -89,7 +42,7 @@ class Solution:
                 left = b + 1
                 right = len(nums) - 1
                 while left < right:
-                    print(a, b, left, right)
+                    # print(a, b, left, right)
                     sum_of_4 = nums[a] + nums[b] + nums[left] + nums[right]
                     if sum_of_4 > target:
                         right -= 1
@@ -105,6 +58,9 @@ class Solution:
                         left += 1
                         right -= 1
         return res
+
+# 时间复杂度 O(n^3)
+# 空间复杂度 O(k) # 跟输入的 n 大小无关
 
 sol = Solution()
 res = sol.fourSum(nums = [-489,-475,-469,-468,-467,-462,-456,-443,-439,-425,-425,-410,-401,-342,-341,-331,-323,-307,-299,-262,-254,-245,-244,-238,-229,-227,-225,-224,-221,-197,-173,-171,-160,-142,-142,-136,-134,-125,-114,-100,-86,-81,-66,-47,-37,-34,4,7,11,34,60,76,99,104,113,117,124,139,141,143,144,146,157,157,178,183,185,189,192,194,221,223,226,232,247,249,274,281,284,293,298,319,327,338,340,368,375,377,379,388,390,392,446,469,480,490], target = 2738)
--- DataStructure_String/459-RepeatedSubstringPattern.py
+++ DataStructure_String/459-RepeatedSubstringPattern.py
@@ -1,61 +1,32 @@
-# class Solutioin():
-#     def repeat(self, s: str):
-#         n = len(s)
-#         for i in range(1, n//2 + 1):
-#             if n % i == 0 and s[:i] * (n//i) == s[:] :
-#                 return True
-        
-#         return False
+# 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
 
-# sol = Solutioin()
-# res = sol.repeat(s = "abab")
-# print(res)
+# 示例 1:
+# 输入: s = "abab"
+# 输出: true
+# 解释: 可由子串 "ab" 重复两次构成。
 
+# 示例 2:
+# 输入: s = "aba"
+# 输出: false
 
-# class Solution():
-#     def repeatedsp(self, s: str):
-#         n = len(s)
-#         if n <= 1:
-#             return False
-        
-#         for i in range(1, (n // 2) + 1):
-#             if n % i == 0 and s[:i] * (n//i) == s:
-#                 return True
-
-#         return False
-
-# sol = Solution()
-# res = sol.repeatedsp(s = "ababb")
-# print(res)
+# 示例 3:
+# 输入: s = "abcabcabcabc"
+# 输出: true
+# 解释: 可由子串 "abc" 重复四次构成。 (或子串 "abcabc" 重复两次构成。)
 
 # 时间复杂度分析：
-
 # 循环从 i = 1 到 (n // 2) + 1 进行迭代，其中 n 表示输入字符串 s 的长度。
 # 在每次迭代中，进行了一些常数时间的操作，包括整数除法，字符串切片，字符串乘法和字符串比较。
 # 循环的迭代次数取决于 n 的大小，但是它不会遍历整个字符串 s。
 # 因为循环的迭代次数与输入规模 n 相关，所以该算法的时间复杂度是 O(n)。
 
 # 空间复杂度分析：
-
 # 除了循环变量 i 和输入参数 s 之外，代码没有使用额外的数据结构或内存空间。
 # 字符串切片和乘法操作不会显式创建新的字符串，因此它们不会显著增加空间复杂度。
 # 因此，该算法的空间复杂度是 O(1)，即它的空间消耗是常数级的，与输入规模无关。
 
 # 2023.9.17(6th)
-# class Solution():
-#     def repeated(self, s: str):
-#         new_s = ''
-#         for i in range(len(s) // 2):
-#             new_s = new_s + s[i]
-#             if new_s * (len(s) // (i+1) ) == s:
-#                 return True
-        
-#         return False
 
-# sol = Solution()
-# res = sol.repeated(s = 'ababb')
-# print(res)
-# # 时间、空间都是 O(n)
 
 import datetime
 
@@ -74,8 +45,10 @@ class Solution():
         n = len(s)
         s_i = ''
         for i in range(1, (n // 2) + 1):
+            # 试每一种可能的子串长度，看能不能整除并完整拼回原串。
             s_i = s[:i]
             # print(s_i, i)
+            # 完整拼回原串: 长度刚好整除, 且重复 n//i 次后刚好一致
             if n % i == 0 and s == s_i * (n // i):
                 return True
         
