656d8c096ff04a7e95b8a2e3c3ec8b6ec8b154a8
wendashi <stoneshaintshi@gmail.com>
2026-08-14 22:24:02 +0800

    2026-08-14 22:23:40 18-1st

--- DataStructure_HashTable/18-4Sum.py
+++ DataStructure_HashTable/18-4Sum.py
@@ -1,153 +1,13 @@
-# class Solution():
-#     def foursum(self, nums: list[int], target: int):
-#         result = []
-#         nums.sort()
-#         n = len(nums)
+# https://leetcode.cn/problems/4sum/description/
+# 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
 
-#         for i in range(n):
-#             if nums[i] > 0 and target > 0:
-#                 break
-
-#             if i > 0 and nums[i] == nums[i - 1]:
-#                 continue
-
-#             for j in range(i + 1, n):
-#                 if nums[j] > 0 and target > 0:
-#                     break
-
-#                 if j > i + 1 and nums[j] == nums[j - 1]:
-#                     continue
-
-#                 left = j + 1
-#                 right = n - 1
-
-#                 while left < right:
-#                     sum4 = nums[i] + nums[j] + nums[left] + nums[right]
-
-#                     if sum4 > target:
-#                         right -= 1
-#                     elif sum4 < target:
-#                         left += 1
-#                     else:
-#                         result.append([nums[i], nums[j], nums[left], nums[right]])
-                        
-#                         while left < right and nums[right] == nums[right - 1]:
-#                             right -= 1
-#                         while left < right and nums[left] == nums[left + 1]:
-#                             left += 1
-#                         right -= 1
-#                         left += 1
-
-#             return result
-
-# nums1 = [1,0,-1,0,-2,2]
-# target1 = 0                    
-
-# sol = Solution()
-# res = sol.foursum(nums1, target1)
-# print(res)
-                    
-
-# class Solution():
-#     def sumof4(self, nums: list[int], target: int):
-#         result = []
-#         nums.sort()
-#         print(nums)
-
-#         for i in range(len(nums)):
-#             if target > 0 and nums[i] > target:
-#                 return result
-            
-#             if i > 0 and nums[i] == nums[i - 1]:
-#                 continue
-
-#             j = i + 1
-#             for j in range(i+1, len(nums)):
-#                 print(i, j)
-#                 if target > 0 and nums[i] + nums[j] > target:
-#                     return result
-
-#                 if j > i + 1  and nums[j] == nums[j - 1]:
-#                     continue
-
-#                 left = j + 1
-#                 right = len(nums) - 1
-#                 while left < right:
-#                     sumof4 = nums[i] + nums[j] + nums[left] + nums[right]
-#                     if sumof4 > target:
-#                         right -= 1
-
-#                     elif sumof4 < target:
-#                         left += 1
-
-#                     else:
-#                         result.append([nums[i], nums[j], nums[left], nums[right]])
-
-#                         while left < right and nums[left] == nums[left + 1]:
-#                             left += 1
-#                         while left < right and nums[right] == nums[right - 1]:
-#                             right -= 1
-#                         left += 1
-#                         right -= 1
-        
-#         return result
-        
-# sol = Solution()
-# res = sol.sumof4(nums = [1,0,-1,0,-2,2], target = 0)
-# print(res)
-
-
-# 2023.9.17 (6th)
-
-# class Solution():
-#     def sumoffour(self, nums: list[int], target: int):
-#         res = []
-#         nums.sort()
-#         # print(nums)
-
-#         for i in range(len(nums)):
-#             # print('nums[i]', nums[i])
-#             if target > 0 and nums[i] > target:
-#                 break
-
-#             if i > 0 and nums[i] == nums[i - 1]:
-#                 continue
-
-#             for j in range(i + 1, len(nums)):
-#                 # print('nums[j]', nums[j])
-#                 if target > 0 and nums[i] + nums[j] > target:
-#                     break
-
-#                 if j > i+1 and nums[j] == nums[j - 1]:
-#                     continue
-
-#                 l = j + 1
-#                 r = len(nums) - 1
-
-#                 while l < r :
-#                     sum = nums[i] + nums[j] + nums[l] + nums[r]
-#                     if sum < target:
-#                         l += 1
-#                     elif sum > target:
-#                         r -= 1
-#                     else:
-#                         res.append([nums[i], nums[j], nums[l], nums[r]])
-
-#                         while l < r and nums[l] == nums[l + 1]:
-#                             l += 1
-#                         while l < r and nums[r] == nums[r - 1]:
-#                             r -= 1
-#                         l += 1
-#                         r -= 1
-        
-#         return res
-    
-# sol = Solution()
-# res = sol.sumoffour(nums = [1,0,-1,0,-2,2], target = 0)
-# print(res)
+# 0 <= a, b, c, d < n
+# a、b、c 和 d 互不相同
+# nums[a] + nums[b] + nums[c] + nums[d] == target
+# 你可以按 任意顺序 返回答案 。
                 
 import datetime
-
+from typing import List
 # 获取当前的日期和时间
 current_time = datetime.datetime.now()
 
@@ -159,52 +19,93 @@ print("当前时间是:", formatted_time)
 
 # 当前时间是: 2023-09-29 21:09:20
 
-class Solution():
-    def foursum(self, nums: list[int], target: int):
+# class Solution():
+#     def foursum(self, nums: list[int], target: int):
+#         nums.sort()
+#         res = []
+#         for i in range(len(nums)):
+#             print('i', nums[i])
+#             if nums[i] > target and target > 0:
+#                 break
+            
+#             # 避免同样的第一个数重复开头
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 continue
+
+#             for j in range(i + 1, len(nums)):
+#                 print('j', nums[j])
+#                 if nums[i] + nums[j] > target and target > 0:
+#                     break
+
+#                 # 避免同样的第二个数重复开头
+#                 if j > i + 1 and nums[j] == nums[j - 1]:
+#                     continue
+
+#                 l = j + 1
+#                 r = len(nums) - 1
+
+#                 # print(l, r)
+#                 while l < r:
+#                     sum = nums[i] + nums[j] + nums[l] + nums[r]
+#                     # print(([nums[i], nums[j], nums[l], nums[r]]))
+#                     # print('sum', sum)
+#                     if sum > target:
+#                         r -= 1
+#                     elif sum < target:
+#                         l += 1
+#                     else:
+#                         # print('???????')
+#                         res.append([nums[i], nums[j], nums[l], nums[r]])
+#                         # print('res', res)
+
+#                         while l < r and nums[l] == nums[l + 1]:
+#                             l += 1
+#                         while l < r and nums[r] == nums[r - 1]:
+#                             r -= 1
+#                         l += 1
+#                         r -= 1
+        
+#         return res
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int):
+        # 双指针
         nums.sort()
         res = []
-        for i in range(len(nums)):
-            print('i', nums[i])
-            if nums[i] > target and target > 0:
+        for a in range(len(nums)):
+            if nums[a] > target and target >= 0:
                 break
 
-            if i > 0 and nums[i] == nums[i - 1]:
+            if a > 0 and nums[a - 1] == nums[a]:
                 continue
 
-            for j in range(i + 1, len(nums)):
-                print('j', nums[j])
-                if nums[i] + nums[j] > target and target > 0:
+            for b in range(a + 1, len(nums)):
+                if nums[b] + nums[a] > target and target >= 0:
                     break
                 
-                if j > i + 1 and nums[j] == nums[j - 1]:
+                if b > a + 1 and nums[b - 1] == nums[b]:
                     continue
 
-                l = j + 1
-                r = len(nums) - 1
-
-                # print(l, r)
-                while l < r:
-                    sum = nums[i] + nums[j] + nums[l] + nums[r]
-                    print(([nums[i], nums[j], nums[l], nums[r]]))
-                    print('sum', sum)
-                    if sum > target:
-                        r -= 1
-                    elif sum < target:
-                        l += 1
+                left = b + 1
+                right = len(nums) - 1
+                while left < right:
+                    print(a, b, left, right)
+                    sum_of_4 = nums[a] + nums[b] + nums[left] + nums[right]
+                    if sum_of_4 > target:
+                        right -= 1
+                    elif sum_of_4 < target:
+                        left += 1
                     else:
-                        print('???????')
-                        res.append([nums[i], nums[j], nums[l], nums[r]])
-                        print('res', res)
+                        res.append([nums[a], nums[b], nums[left], nums[right]])
 
-                        while l < r and nums[l] == nums[l + 1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r - 1]:
-                            r -= 1
-                        l += 1
-                        r -= 1
-        
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
         return res
 
 sol = Solution()
-res = sol.foursum(nums = [-3,-4,5,5,-5], target = 2)
+res = sol.fourSum(nums = [-489,-475,-469,-468,-467,-462,-456,-443,-439,-425,-425,-410,-401,-342,-341,-331,-323,-307,-299,-262,-254,-245,-244,-238,-229,-227,-225,-224,-221,-197,-173,-171,-160,-142,-142,-136,-134,-125,-114,-100,-86,-81,-66,-47,-37,-34,4,7,11,34,60,76,99,104,113,117,124,139,141,143,144,146,157,157,178,183,185,189,192,194,221,223,226,232,247,249,274,281,284,293,298,319,327,338,340,368,375,377,379,388,390,392,446,469,480,490], target = 2738)
 print(res)
